8847b0c92825bbdd7f78cbad41c8913623ade334
zorro199 <zaur.brayn@mail.ru>
2024-06-07 01:11:16 +0300

    init06

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { HashRouter, Route, Routes } from 'react-router-dom'
+import { RouterProvider, createHashRouter } from 'react-router-dom'
 import About from './pages/About.tsx'
+
+
+const router = createHashRouter([
+  {
+    path: "/market/",
+    element: <App />,
+    children: [
+      {
+        path: "/market/",
+        element: <App />,
+      },
+      {
+        path: "/market/about",
+        element: <About />,
+      },
+    ],
+  },
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-     <HashRouter basename='/market/'>
-     <Routes>
-        <Route path='/market/' element={ <App/> }/>
-        <Route path='/market/about' element={ <About/> }/>
-      </Routes>
-     </HashRouter>
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
