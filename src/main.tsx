9d3af1e55501d0da06770e59fa2fdcccc9573e4b
zorro199 <zaur.brayn@mail.ru>
2024-06-07 17:54:11 +0300

    08

--- src/main.tsx
+++ src/main.tsx
@@ -2,31 +2,37 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { RouterProvider, createHashRouter } from 'react-router-dom'
+import { HashRouter, Route, Routes} from 'react-router-dom'
 import About from './pages/About.tsx'
 
 
-const router = createHashRouter([
-  {
-    path: "#/market/",
-    element: <App />,
-    children: [
-      {
-        path: "#/market/",
-        element: <App />,
-      },
-      {
-        path: "#/market/about",
-        element: <About />,
-      },
-    ],
-  },
-])
+// const router = createHashRouter([
+//   {
+//     path: "#/market/",
+//     element: <App />,
+//     children: [
+//       {
+//         path: "#/market/",
+//         element: <App />,
+//       },
+//       {
+//         path: "#/market/about",
+//         element: <About />,
+//       },
+//     ],
+//   },
+// ])
 
 
 export const rootElement = ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    {/* <RouterProvider router={router}/> */}
+    <HashRouter basename="/market">
+      <Routes>
+        <Route path="/market/" element={<App />}/>
+        <Route path="/market/#/about" element={<About />}/>
+      </Routes>
+    </HashRouter>
   </React.StrictMode>,
 )
 
