b384a79a80cdfbaafe1dd0247577703cc98be4a2
zorro199 <zaur.brayn@mail.ru>
2024-06-07 18:29:55 +0300

    16

--- src/main.tsx
+++ src/main.tsx
@@ -2,37 +2,37 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import About from './pages/About.tsx'
 
 
-const router = createBrowserRouter([
-  {
-    path: "/market/",
-    element: <App />,
-    children: [
-      {
-        path: "/market/",
-        element: <App />,
-      },
-      {
-        path: "/market/about",
-        element: <About />,
-      },
-    ],
-  },
-])
+// const router = createBrowserRouter([
+//   {
+//     path: "/market/",
+//     element: <App />,
+//     children: [
+//       {
+//         path: "/market/",
+//         element: <App />,
+//       },
+//       {
+//         path: "/market/about",
+//         element: <About />,
+//       },
+//     ],
+//   },
+// ])
 
 
 export const rootElement = ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
-    {/* <HashRouter basename="/market/">
+    {/* <RouterProvider router={router}/> */}
+    <BrowserRouter basename="/market/">
       <Routes>
         <Route path="/market/" element={<App />}/>
-        <Route path="/market/#/about" element={<About />}/>
+        <Route path="/market/about" element={<About />}/>
       </Routes>
-    </HashRouter> */}
+    </BrowserRouter>
   </React.StrictMode>,
 )
 
