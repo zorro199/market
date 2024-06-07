a0edef127e1c2776cd0afe97a3058b628a4238f3
zorro199 <zaur.brayn@mail.ru>
2024-06-07 18:32:19 +0300

    17

--- src/main.tsx
+++ src/main.tsx
@@ -2,37 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import About from './pages/About.tsx'
 
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/market/",
-//     element: <App />,
-//     children: [
-//       {
-//         path: "/market/",
-//         element: <App />,
-//       },
-//       {
-//         path: "/market/about",
-//         element: <About />,
-//       },
-//     ],
-//   },
-// ])
+const router = createBrowserRouter([
+  {
+    path: "/market/",
+    element: <App />,
+    children: [
+      {
+        path: "/market/",
+        element: <App />,
+      },
+      {
+        path: "/market/about",
+        element: <About />,
+      },
+    ],
+  },
+])
 
 
 export const rootElement = ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* <RouterProvider router={router}/> */}
-    <BrowserRouter basename="/market/">
-      <Routes>
-        <Route path="/market/" element={<App />}/>
-        <Route path="/market/about" element={<About />}/>
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
 
