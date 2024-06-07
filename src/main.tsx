c21835d522b7eab9919cfaff744df63ac3204c81
zorro199 <zaur.brayn@mail.ru>
2024-06-07 17:36:07 +0300

    06

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createHashRouter } from 'react-router-dom'
 import About from './pages/About.tsx'
 
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/market/",
     element: <App />,
